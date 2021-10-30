fcc0461178665d9def80c92f37b5b796b4bbb8fa
Kuchteq <maniuchta3@gmail.com>
2021-10-30 14:59:17 +0200

    Completed sellModal

--- src/lib/modals/SellModal.svelte
+++ src/lib/modals/SellModal.svelte
@@ -9,10 +9,11 @@
 	import selectedComputers from '$functions/cSelectionManager';
 	import createPartSeller from '$functions/sellPartsClient.js';
 	import createComputerSeller from '$functions/sellComputersClient.js';
-
+	import { writable } from 'svelte/store';
 	import { closeModal } from '$functions/modalManager';
-	import SellPartChunk from '$lib/SellChunks/SellPartChunk.svelte';
-	import SellComputerChunk from '$lib/SellChunks/SellComputerChunk.svelte';
+	import SellPartChunk from '$lib/sellComps/SellPartChunk.svelte';
+	import SellComputerChunk from '$lib/sellComps/SellComputerChunk.svelte';
+	import MoreAddButton from '$lib/sellComps/MoreAddButton.svelte';
 
 	const SERVER_ERROR_STRING =
 		'Serwer nie mógł przetworzyć tej operacji, możliwy błąd w uzupełnionych danych';
@@ -22,6 +23,7 @@
 	let client = createPostClient(orderForm);
 	let seller = createPartSeller();
 	let coSeller = createComputerSeller();
+	let waitingState = writable(false);
 
 	let actionButton = {
 		do: () => {
@@ -80,42 +82,67 @@
 	$: coSeller.synchronize($selectedComputers);
 </script>
 
-<UniModal {modalName} theme="sellModal" {actionButton} {resetAction} tabName="Sprzedaj">
-	<form bind:this={formRef}>
-		{#each $client as field, id}
-			<svelte:component
-				this={field.component}
-				{id}
-				label={field.label}
-				update={client.updateVal}
-				required={field.required}
-				initValue={field.value}
-				fetchString={field.fetchString && field.fetchString}
-				placeholder={field.placeholder && field.placeholder}
-				themeColor={field.themeColor && field.themeColor}
-				addHandlerModal={field.addHandlerModal && field.addHandlerModal}
-				boundries={field.boundries || undefined}
-				error={field.error || undefined}
+<div class={$waitingState ? 'awaitForNewParts' : ''}>
+	<UniModal {modalName} theme="sellModal" {actionButton} {resetAction} tabName="Sprzedaj">
+		<form bind:this={formRef}>
+			{#each $client as field, id}
+				<svelte:component
+					this={field.component}
+					{id}
+					label={field.label}
+					update={client.updateVal}
+					required={field.required}
+					initValue={field.value}
+					fetchString={field.fetchString && field.fetchString}
+					placeholder={field.placeholder && field.placeholder}
+					themeColor={field.themeColor && field.themeColor}
+					addHandlerModal={field.addHandlerModal && field.addHandlerModal}
+					boundries={field.boundries || undefined}
+					error={field.error || undefined}
+				/>
+			{/each}
+		</form>
+		<form class="sellPartChunks">
+			<h1>Wybrane części do sprzedania</h1>
+			{#if $seller.length > 0}
+				{#each $seller as item, id}
+					<SellPartChunk chunkId={id} part={item} sellFunc={seller.updateVal} />
+				{/each}
+			{/if}
+			<MoreAddButton
+				color="blueScheme"
+				text="Dodaj części"
+				href="/inventory"
+				icon="/icons/InventoryModuleIcon.svg"
+				state={waitingState}
 			/>
-		{/each}
-	</form>
-	<form class="sellPartChunks">
-		<h1>Wybrane części do sprzedania</h1>
-		{#if $seller.length > 0}
-			{#each $seller as item, id}
-				<SellPartChunk chunkId={id} part={item} sellFunc={seller.updateVal} />
-			{/each}
-		{/if}
-	</form>
-	<form class="sellComputerChunks">
-		<h1>Wybrane komputery do sprzedania</h1>
-		{#if $coSeller.length > 0}
-			{#each $coSeller as item, id}
-				<SellComputerChunk chunkId={id} sellData={item} sellFunc={coSeller.updateVal} />
-			{/each}
-		{/if}
-	</form>
-</UniModal>
+		</form>
+		<form class="sellComputerChunks">
+			<h1>Wybrane komputery do sprzedania</h1>
+			{#if $coSeller.length > 0}
+				{#each $coSeller as item, id}
+					<SellComputerChunk chunkId={id} sellData={item} sellFunc={coSeller.updateVal} />
+				{/each}
+			{/if}
+			<MoreAddButton
+				color="purpleScheme"
+				text="Dodaj zestawy"
+				href="/computers"
+				icon="/icons/ComputersModuleIcon.svg"
+				state={waitingState}
+			/>
+		</form>
+	</UniModal>
+</div>
+<button
+	type="button"
+	class="returnToModalBtn {$waitingState ? '' : 'hiddenBtn'}"
+	on:click={() => {
+		waitingState.set(false);
+	}}
+>
+	<img src="/icons/ExpandToTable.svg" />Powróć do zamówienia
+</button>
 
 <style lang="scss">
 	:global(.uniModal.sellModal) {
@@ -141,5 +168,50 @@
 		h1 {
 			color: var(--mPurple);
 		}
+	}
+	@keyframes hideForNewParts {
+		0% {
+			display: block;
+			visibility: visible;
+			opacity: 1;
+		}
+		99% {
+			display: block;
+			visibility: visible;
+			opacity: 0;
+		}
+		100% {
+			display: none;
+			visibility: hidden;
+			opacity: 0;
+		}
+	}
+	.awaitForNewParts {
+		animation: hideForNewParts 0.2s;
+		animation-fill-mode: forwards;
+	}
+	.returnToModalBtn {
+		position: fixed;
+		bottom: 0;
+		left: 50%;
+		transform: translate(-50%, 0);
+		z-index: 110;
+		background-color: #ff5b5b;
+		display: flex;
+		align-items: center;
+		font-size: 18px;
+		color: #fff;
+		font-weight: 700;
+		padding: 10px 50px;
+		border-radius: 20px 20px 0 0px;
+		transition: var(--sTrans);
+		opacity: 1;
+		img {
+			margin-right: 15px;
+		}
+	}
+	.hiddenBtn {
+		display: none;
+		opacity: 0;
 	}
 </style>
---
+++ src/lib/sellComps/MoreAddButton.svelte
@@ -0,0 +1,42 @@
+<script>
+	export let text = '';
+	export let href = '';
+	export let icon = '';
+	export let color = '';
+	export let state;
+</script>
+
+<a
+	class={color}
+	{href}
+	on:click={() => {
+		state.set(true);
+	}}
+	>{text}
+	<img src={icon} />
+</a>
+
+<style lang="scss">
+	a {
+		color: #fff;
+		padding: 10px 30px;
+		border-radius: 30px;
+		display: flex;
+		align-items: center;
+		width: fit-content;
+		font-weight: 600;
+		margin: 20px auto 5px;
+	}
+	a img {
+		margin-left: 10px;
+	}
+	.blueScheme {
+		background: var(--mBlue);
+	}
+	.purpleScheme {
+		background: var(--mPurple);
+		img {
+			width: 24px;
+		}
+	}
+</style>
